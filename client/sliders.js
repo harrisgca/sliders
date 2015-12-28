c258311d5cd4a27deaf42b231f221a3b747bb342
Glenn <harris.glenn.jr@gmail.com>
2015-12-27 22:19:55 -0800

    sliders for RGB values are working

--- client/sliders.js
+++ client/sliders.js
@@ -1,14 +1,48 @@
-Session.setDefault('counter', 0);
+Session.setDefault('redValue', 0);
+Session.setDefault('greenValue', 0);
+Session.setDefault('blueValue', 0);
 
-Template.hello.helpers({
-  counter: function () {
-    return Session.get('counter');
+var redChange = function(event){
+  if (event.target.name === 'red') {
+    return true;
+  }
+}
+
+var greenChange = function(event){
+  if (event.target.name === 'green') {
+    return true;
+  }
+}
+
+var blueChange = function(event){
+  if (event.target.name === 'blue') {
+    return true;
+  }
+}
+
+Template.content.helpers({
+  redValue: function(){
+    return Session.get('redValue');
+  },
+  greenValue: function(){
+    return Session.get('greenValue');
+  },
+  blueValue: function(){
+    return Session.get('blueValue');
   }
 });
 
-Template.hello.events({
-  'click button': function () {
-    // increment the counter when button is clicked
-    Session.set('counter', Session.get('counter') + 1);
+Template.slider.events({
+  'input': function(event){
+    // console.log(event.target.value);
+    // console.log(event.target.name);
+    // Session.set('slideValue', event.target.value)
+    if (redChange(event)) {
+      Session.set('redValue', event.target.value);
+    }else if (greenChange(event)) {
+      Session.set('greenValue', event.target.value);
+    }else if (blueChange(event)) {
+      Session.set('blueValue', event.target.value);
+    }
   }
 });
